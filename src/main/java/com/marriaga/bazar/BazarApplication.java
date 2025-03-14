dca63753bcc8096d90ffb9c695012f8641f9c759
Miguel Arriaga <miguel.arriaga.arellano@gmail.com>
2025-03-14 17:40:21 -0600

    Agregando las variables de entorno

--- src/main/java/com/marriaga/bazar/BazarApplication.java
+++ src/main/java/com/marriaga/bazar/BazarApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BazarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BazarApplication.class, args);
-	}
-
+		SpringApplication.run(BazarApplication.class, args);}
 }
